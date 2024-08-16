fe6150c654e83d4eca41dd79711ae9804fd58aaf
Nehasimon <108783638+Nehasimon@users.noreply.github.com>
2024-08-16 17:08:38 -0400

    Update s3UI.py

--- s3UI.py
+++ s3UI.py
@@ -160,7 +160,7 @@ input_features = health_values_binary + [age_days, ethnicity_encoded_value, reli
                                          insurance_encoded, admission_type_encoded, 
                                          marital_status_encoded, selected_gender]
 
-input_features += [0] * (53 - len(input_features))  # Ensure the feature vector has 48 elements
+input_features += [0] * (48 - len(input_features))  # Ensure the feature vector has 48 elements
 #input_features = numpy.array(input_features).reshape(1, -1)
 
 # Submit button
