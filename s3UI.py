83e90078cba2bb53cc061f166a182fa7621c87e1
Nehasimon <108783638+Nehasimon@users.noreply.github.com>
2024-10-07 11:33:39 -0400

    Update s3UI.py

--- s3UI.py
+++ s3UI.py
@@ -54,119 +54,119 @@ st.markdown(
 
 st.title("Hospital Length of Stay Prediction")
 
-# S3 bucket and model key
-bucket_name = 'mod-los-test2'
-model_key = 'tabular/model/x.joblib'
+# # S3 bucket and model key
+# bucket_name = 'mod-los-test2'
+# model_key = 'tabular/model/x.joblib'
 
-# Load the model from S3
-model = load_model_from_s3(bucket_name, model_key)
+# # Load the model from S3
+# model = load_model_from_s3(bucket_name, model_key)
 
-# Define feature lists
-health_categories = {
-    "Hematological Concerns": ["blood"],
-    "Circulatory Concerns": ["circulatory"],
-    "Congenital Conditions": ["congenital"],
-    "Digestive Concerns": ["digestive"],
-    "Endocrine Concerns": ["endocrine"],
-    "Genitourinary Concerns": ["genitourinary"],
-    "Infectious Diseases": ["infectious"],
-    "Injury Concerns": ["injury"],
-    "Mental Health": ["mental"],
-    "Miscellaneous Conditions": ["misc"],
-    "Muscular Concerns": ["muscular"],
-    "Neoplasms (Tumors)": ["neoplasms"],
-    "Nervous System Concerns": ["nervous"],
-    "Prenatal Conditions": ["prenatal"],
-    "Respiratory Concerns": ["respiratory"],
-    "Skin Conditions": ["skin"]
-}
+# # Define feature lists
+# health_categories = {
+#     "Hematological Concerns": ["blood"],
+#     "Circulatory Concerns": ["circulatory"],
+#     "Congenital Conditions": ["congenital"],
+#     "Digestive Concerns": ["digestive"],
+#     "Endocrine Concerns": ["endocrine"],
+#     "Genitourinary Concerns": ["genitourinary"],
+#     "Infectious Diseases": ["infectious"],
+#     "Injury Concerns": ["injury"],
+#     "Mental Health": ["mental"],
+#     "Miscellaneous Conditions": ["misc"],
+#     "Muscular Concerns": ["muscular"],
+#     "Neoplasms (Tumors)": ["neoplasms"],
+#     "Nervous System Concerns": ["nervous"],
+#     "Prenatal Conditions": ["prenatal"],
+#     "Respiratory Concerns": ["respiratory"],
+#     "Skin Conditions": ["skin"]
+# }
 
-admission_type_options = ["Select",
-    "AMBULATORY OBSERVATION", "DIRECT EMER.", "DIRECT OBSERVATION", 
-    "EU observation", "Elective", "Emergency", "Observation admit", 
-    "Surgical same day admission", "Urgent"
-]
+# admission_type_options = ["Select",
+#     "AMBULATORY OBSERVATION", "DIRECT EMER.", "DIRECT OBSERVATION", 
+#     "EU observation", "Elective", "Emergency", "Observation admit", 
+#     "Surgical same day admission", "Urgent"
+# ]
 
-marital_status_options = ["Select",
-    "DIVORCED", "MARRIED", "SEPARATED", "SINGLE", 
-    "UNKNOWN (DEFAULT)", "WIDOWED"
-]
+# marital_status_options = ["Select",
+#     "DIVORCED", "MARRIED", "SEPARATED", "SINGLE", 
+#     "UNKNOWN (DEFAULT)", "WIDOWED"
+# ]
 
-gender_options = {"Male": 0, "Female": 1}
+# gender_options = {"Male": 0, "Female": 1}
 
-insurance_options = ["Select","Medicaid", "Medicare", "Private", "Other"]
-religion_options = ["Select", "RELIGIOUS", "UNOBTAINABLE","NOT SPECIFIED"]
-ethnicity_options = ["Select","BLACK/AFRICAN AMERICAN", "HISPANIC/LATINO", "ASIAN", "WHITE", "UNKNOWN"]
+# insurance_options = ["Select","Medicaid", "Medicare", "Private", "Other"]
+# religion_options = ["Select", "RELIGIOUS", "UNOBTAINABLE","NOT SPECIFIED"]
+# ethnicity_options = ["Select","BLACK/AFRICAN AMERICAN", "HISPANIC/LATINO", "ASIAN", "WHITE", "UNKNOWN"]
 
-ethnicity_mapping = {option: index for index, option in enumerate(ethnicity_options)}
+# ethnicity_mapping = {option: index for index, option in enumerate(ethnicity_options)}
 
-# Streamlit UI
-with st.container():
-    st.header("Patient Information")
-    selected_admission_type = st.selectbox("Select Admission Type", admission_type_options, index=0)
-    selected_marital_status = st.selectbox("Select Marital Status", marital_status_options, index=0)
-    selected_gender_text = st.selectbox("Select Gender", list(gender_options.keys()), index=0)
-    selected_gender = gender_options[selected_gender_text]
-    selected_insurance = st.selectbox("Select Insurance Type", insurance_options, index=0)
-    selected_religion = st.selectbox("Select Religion", religion_options, index=0)
+# # Streamlit UI
+# with st.container():
+#     st.header("Patient Information")
+#     selected_admission_type = st.selectbox("Select Admission Type", admission_type_options, index=0)
+#     selected_marital_status = st.selectbox("Select Marital Status", marital_status_options, index=0)
+#     selected_gender_text = st.selectbox("Select Gender", list(gender_options.keys()), index=0)
+#     selected_gender = gender_options[selected_gender_text]
+#     selected_insurance = st.selectbox("Select Insurance Type", insurance_options, index=0)
+#     selected_religion = st.selectbox("Select Religion", religion_options, index=0)
 
-# Process ethnicity selection
-selected_ethnicity = st.multiselect("Select Ethnicity", ethnicity_options, default=[])
-if "ASIAN" in selected_ethnicity:
-    selected_ethnicity = [e for e in selected_ethnicity if e != "ASIAN"]
-    selected_ethnicity.append("UNKNOWN")
+# # Process ethnicity selection
+# selected_ethnicity = st.multiselect("Select Ethnicity", ethnicity_options, default=[])
+# if "ASIAN" in selected_ethnicity:
+#     selected_ethnicity = [e for e in selected_ethnicity if e != "ASIAN"]
+#     selected_ethnicity.append("UNKNOWN")
 
-ethnicity_encoded_values = [ethnicity_mapping.get(e, 4) for e in selected_ethnicity]
-ethnicity_encoded_value = ethnicity_encoded_values[0] if ethnicity_encoded_values else 4
+# ethnicity_encoded_values = [ethnicity_mapping.get(e, 4) for e in selected_ethnicity]
+# ethnicity_encoded_value = ethnicity_encoded_values[0] if ethnicity_encoded_values else 4
 
-# Input for Age
-age_years = st.text_input("Age", value="")
-try:
-    age_years = int(age_years) if age_years else 0
-except ValueError:
-    st.error("Please enter a valid number for Age.")
-    age_years = 0
-age_days = age_years * 365
-
-
-# Initialize health_values dictionary
-health_values = {category: False for category in health_categories}
-
-# Checklist for health-related categories in two columns
-st.header("Medical Diagnosis")
-col1, col2 = st.columns(2)
-
-with col1:
-    for category in list(health_categories.keys())[:8]:
-        health_values[category] = st.checkbox(category, value=False)
-
-with col2:
-    for category in list(health_categories.keys())[8:]:
-        health_values[category] = st.checkbox(category, value=False)
-
-# Convert category selections to feature vector
-health_values_binary = [int(health_values[category]) for category in health_categories]
+# # Input for Age
+# age_years = st.text_input("Age", value="")
+# try:
+#     age_years = int(age_years) if age_years else 0
+# except ValueError:
+#     st.error("Please enter a valid number for Age.")
+#     age_years = 0
+# age_days = age_years * 365
 
 
-# Convert categorical inputs to numerical values
-admission_type_encoded = admission_type_options.index(selected_admission_type) if selected_admission_type != "Select" else 0
-marital_status_encoded = marital_status_options.index(selected_marital_status) if selected_marital_status != "Select" else 0
-insurance_encoded = insurance_options.index(selected_insurance) if selected_insurance != "Select" else 0
-religion_encoded = religion_options.index(selected_religion) if selected_religion != "Select" else 0
+# # Initialize health_values dictionary
+# health_values = {category: False for category in health_categories}
 
-# Combine features
-input_features = health_values_binary + [age_days, ethnicity_encoded_value, religion_encoded, 
-                                         insurance_encoded, admission_type_encoded, 
-                                         marital_status_encoded, selected_gender]
+# # Checklist for health-related categories in two columns
+# st.header("Medical Diagnosis")
+# col1, col2 = st.columns(2)
 
-input_features += [0] * (48 - len(input_features))  # Ensure the feature vector has 48 elements
-#input_features = numpy.array(input_features).reshape(1, -1)
+# with col1:
+#     for category in list(health_categories.keys())[:8]:
+#         health_values[category] = st.checkbox(category, value=False)
 
-# Submit button
-submit_predict = st.button("Predict")
+# with col2:
+#     for category in list(health_categories.keys())[8:]:
+#         health_values[category] = st.checkbox(category, value=False)
 
-if submit_predict:
-    # Make a prediction using the model
-    prediction = model.predict([input_features])[0]
-    pred = round(prediction)
-    st.write(f"Your estimated stay in the hospital is: {pred} Days.")
+# # Convert category selections to feature vector
+# health_values_binary = [int(health_values[category]) for category in health_categories]
+
+
+# # Convert categorical inputs to numerical values
+# admission_type_encoded = admission_type_options.index(selected_admission_type) if selected_admission_type != "Select" else 0
+# marital_status_encoded = marital_status_options.index(selected_marital_status) if selected_marital_status != "Select" else 0
+# insurance_encoded = insurance_options.index(selected_insurance) if selected_insurance != "Select" else 0
+# religion_encoded = religion_options.index(selected_religion) if selected_religion != "Select" else 0
+
+# # Combine features
+# input_features = health_values_binary + [age_days, ethnicity_encoded_value, religion_encoded, 
+#                                          insurance_encoded, admission_type_encoded, 
+#                                          marital_status_encoded, selected_gender]
+
+# input_features += [0] * (48 - len(input_features))  # Ensure the feature vector has 48 elements
+# #input_features = numpy.array(input_features).reshape(1, -1)
+
+# # Submit button
+# submit_predict = st.button("Predict")
+
+# if submit_predict:
+#     # Make a prediction using the model
+#     prediction = model.predict([input_features])[0]
+#     pred = round(prediction)
+#     st.write(f"Your estimated stay in the hospital is: {pred} Days.")
