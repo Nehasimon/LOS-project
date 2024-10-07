d69d1b36fd8c44dbdede4e6ca68e229603a7fad0
Nehasimon <108783638+Nehasimon@users.noreply.github.com>
2024-10-07 11:32:00 -0400

    Update s3UI.py

--- s3UI.py
+++ s3UI.py
@@ -55,8 +55,8 @@ st.markdown(
 st.title("Hospital Length of Stay Prediction")
 
 # S3 bucket and model key
-bucket_name = 'med-aifactory'
-model_key = 'tabular/model/model.joblib'
+bucket_name = 'mod-los-test2'
+model_key = 'tabular/model/x.joblib'
 
 # Load the model from S3
 model = load_model_from_s3(bucket_name, model_key)
